947b5da10f6cfc56d2aca72d77382dc2916718d1
Gabriel <127807744+gabriel-dev1@users.noreply.github.com>
2023-05-24 19:13:45 -0300

    Upload

--- script.ts
+++ script.ts
@@ -6,22 +6,8 @@ function dizOi(name: string)  {
     return `Olá ${name}`
 }
 
-const result = multiplicar(4,6)
+const result = multiplicar(20,40)
 console.log(result)
 
 const result2 = dizOi('Roger')
 console.log(result2)
-
-//---------------
-
-const teste1 = (a:number, b:number) => {
-    return a * b
-}
-
-const teste2 = (name:string) => {
-    return `oi ${name}`
-}
-
-console.log(teste1(2,3))
-
-console.log(teste2('carlos'))
