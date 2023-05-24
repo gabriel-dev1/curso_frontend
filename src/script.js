8ab904b855584851e5b9e3e16c0bea602e65e0b3
Gabriel <127807744+gabriel-dev1@users.noreply.github.com>
2023-05-24 11:46:13 -0300

    Upload
    Exercício sobre os recursos do ES6+

---
+++ src/script.js
@@ -0,0 +1,23 @@
+const students = [
+    {nome: 'Maria', nota: 2},
+    {nome: 'Roger', nota: 7},
+    {nome: 'Marcos', nota: 4},
+    {nome: 'Jose', nota: 1,},
+    {nome: 'Luana', nota: 9},
+    {nome: 'Carla,', nota: 6},
+    {nome: 'Lucas', nota: 6},
+    {nome: 'Joao', nota: 3}
+]
+
+const notasAltas = students.filter((student) => {
+    return student.nota >= 6
+})
+
+const notasBaixas = students.filter((student) => {
+    return student.nota <=5 
+})
+
+console.log(students)
+console.log(notasAltas)
+console.log(notasBaixas)
+
